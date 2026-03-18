b5a2d85a19d26e20276bbadf65a76e5f0a791051
Mirza Abdulahovic <mirzaabdulahovic1@gmail.com>
2026-03-18 15:52:41 +0100

    chore: Redo Jest configuration

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -2,11 +2,18 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  roots: ['<rootDir>/src'],
+  roots: ['<rootDir>/src', '<rootDir>/tests'],
   testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
   transform: {
-    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.test.json',
+      },
+    ],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  setupFiles: ['<rootDir>/tests/setup/env.setup.ts'],
+  setupFilesAfterEnv: ['<rootDir>/tests/setup/jest.setup.ts'],
   clearMocks: true,
 };
